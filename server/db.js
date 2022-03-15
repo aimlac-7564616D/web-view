64c44cb6154275c1955d3a05e39e1c253ba3956c
Ding Sheng Ong <dso@aber.ac.uk>
2022-03-15 21:44:06 +0000

    added procedure to verify database connection

--- server/db.js
+++ server/db.js
@@ -11,11 +11,32 @@ const dbConfig = {
 }
 
 const conn = mysql.createConnection(dbConfig)
-conn.connect(err => {
-  if (err) throw err
-  let dbURL = `${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`
-  console.log(`Connected to database: ${dbURL}`)
-})
+
+const establishConnection = () => (
+  new Promise((resolve, reject) => {
+    conn.connect(err => {
+      if (err) {
+        reject(err)
+        return
+      }
+      let dbURL = `${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`
+      console.log(`Connected to database: ${dbURL}`)
+      resolve(true)
+    })
+  })
+)
+
+const connect = async (app) => {
+  let connected = false
+  while (!connected) {
+    try {
+      connected = await establishConnection()
+    } catch ( error ) {}
+    // wait for 1s
+    await new Promise(r => setTimeout(r, 1000))
+  }
+  app.emit('ready')
+}
 
 const query = (sql, params) => {
   return new Promise((resolve, reject) => {
@@ -35,6 +56,7 @@ const queryOne = async (sql, params) => {
 }
 
 module.exports = {
+  connect: connect,
   query: query,
   queryOne: queryOne,
   type: mysql.Types
